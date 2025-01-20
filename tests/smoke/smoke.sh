c216c6880ccf1e7ba9057df193e78a5838d3f09e
Pino Toscano <ptoscano@redhat.com>
2025-01-20 13:33:11 +0100

    tests: fix file check with newer cockpit
    With newer versions of Cockpit the HTML files are compressed, so adapt
    the existence check to that.
    
    While there, enable tracking in the script so it is possible to see what
    is run and what fails (otherwise a failure produces no output).

--- tests/smoke/smoke.sh
+++ tests/smoke/smoke.sh
@@ -2,7 +2,8 @@
 
 set -e
 set -o pipefail
+set -x
 
 # Check that plugin is installed in expected location and required files are present
-file /usr/share/cockpit/subscription-manager/index.html | grep -q "HTML document"
+file /usr/share/cockpit/subscription-manager/index.html.gz | grep -q "gzip compressed data"
 file /usr/share/cockpit/subscription-manager/manifest.json | grep -q "JSON"
