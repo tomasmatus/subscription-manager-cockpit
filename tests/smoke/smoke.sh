530c3cf4353791464b614c42239b9c9b5b4a76bc
Jiri Hnidek <jhnidek@redhat.com>
2025-07-22 14:49:13 +0200

    fix: The index.html is not gzipped in new cockpit
    * The new version of cockpit does not gzip index.html file,
      because this file is too small. Thus, fix our smoke test
      to consider this change
    
    Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

--- tests/smoke/smoke.sh
+++ tests/smoke/smoke.sh
@@ -5,5 +5,5 @@ set -o pipefail
 set -x
 
 # Check that plugin is installed in expected location and required files are present
-file /usr/share/cockpit/subscription-manager/index.html.gz | grep -q "gzip compressed data"
+file /usr/share/cockpit/subscription-manager/index.html | grep -q "HTML document, ASCII text"
 file /usr/share/cockpit/subscription-manager/manifest.json | grep -q "JSON"
